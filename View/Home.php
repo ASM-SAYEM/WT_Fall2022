d8afacc69421013c08019d0cb4b910caa89557c5
ASM-SAYEM <114211918+ASM-SAYEM@users.noreply.github.com>
2022-11-16 23:56:23 +0600

    FINAL_LAB_TASK1

---
+++ View/Home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+
+</head>
+<html>
+  <body align="center">
+  <img src="../image/Home.jpg" alt="home picture" width="400" height="400">
+  <h1 align="center"><b><i>WELCOME TO OUR N-BANGLA HOSPITAL'S WEBSITE </b></i></h1>
+          
+
+  <tr align="center">
+    <td><b>"To be a renowned organization at the leading edge of Medicine, providing quality</b></td>
+            <td><b> healthcare to meet our nation's aspirations."</b></td>
+</tr>
+
+
+  <br><a href="../View/PatientRegistration.php"><b>Registration</b></a> <br>
+  
+  <br><a href="../View/PatientLogin.php"><b>Do you have an account? LogIn</b></a> <br>
+  <br><a href="../View/Contact.php"><b>Contact With Us<b></a>
+  <br><a href="../View/About.php"><b>About Us<b></a>
+
+  <?php
+   include ("Footer.php");
+  ?>
+
+  
+  </body>
+      
+     </html>
---
+++ View/PatientProfile.php
@@ -0,0 +1,30 @@
+<?php
+include("../Controll/profile.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body align="center">
+<img src="../image/afterlogin1.jpg" alt="logo" width="300" height="400">
+
+</body>
+<body>
+<h1 align="center"><b><i>WELCOME TO OUR E-HEALTH WEBSITE</b></i></h1>
+ 
+  
+  <br><a href="../View/DoctorsList.php"><b>Doctors List</b></a> <br>
+  <br><a href="../View/Fees.php"><b>Appointment Fee & Others</b></a> <br>
+  <br><a href="../View/Appointment.php"><b>Request For an Appointment</b></a> <br>
+  <br><a href="../View/Ambulance.php"><b>Call Ambulance</b></a> <br>
+
+  <br><a href="../View/Patientlogin.php"><b>LogOut</b></a> <br>
+  <?php
+   include ("Footer.php");
+  ?>
+</body>
+</html>
